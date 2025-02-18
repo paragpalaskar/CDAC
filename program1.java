6cf15fc0e29517f720ba62d0cad9a0c7daaf5aec
Parag Palaskar <133798447+paragpalaskar@users.noreply.github.com>
2025-02-18 21:17:46 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,10 @@
+// 1.   Write a Java program to print Hello on the screen and print your name in separate line
+
+class program1
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello");
+        System.out.println("Parag Palaskar");    
+    }
+}
